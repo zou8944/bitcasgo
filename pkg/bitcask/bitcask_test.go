426cb66338e0f3ff499954d92443c42607e92747
Floyd <894474057@qq.com>
2023-02-20 20:19:54 +0800

    test: Test put

--- pkg/bitcask/bitcask_test.go
+++ pkg/bitcask/bitcask_test.go
@@ -1,17 +1,27 @@
 package bitcask
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
 func TestBitCask_Put(t *testing.T) {
 	basedir := "/Users/zouguodong/Code/Personal/bitcasgo"
-	b, err := New(basedir, "bitcask")
+	b, err := New[int, float32](basedir, "bitcask")
+	defer func() {
+		_ = os.RemoveAll(basedir + "/bitcask-1.bin")
+	}()
 	if err != nil {
 		t.Fatal(err)
 	}
-	err = b.Put(1, 2)
+	err = b.Put(1, 1.1)
 	if err != nil {
 		t.Fatal(err)
 	}
+	value, err := b.Get(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("%f\n", value)
 }
